dbe526a8092e7d8d74ab4b9948ee8d90becf05c6
susiejojo <susiejojo1@gmail.com>
2025-06-16 16:21:48 -0400

    Convert token strings to token id ints

--- sim/request.go
+++ sim/request.go
@@ -19,8 +19,8 @@ type Request struct {
 	ID          string // Unique identifier for the request
 	ArrivalTime int64  // Timestamp when the request enters the system
 
-	InputTokens  []string // Prompt tokens
-	OutputTokens []string // Pre-specified output tokens (already known for the simulation)
+	InputTokens  []int // Prompt tokens
+	OutputTokens []int // Pre-specified output tokens (already known for the simulation)
 
 	State         string // "queued", "running", "completed"
 	ProgressIndex int    // Total number of input tokens processed so far + number of output tokens generated so far
