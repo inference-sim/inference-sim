7f9c880776044230de41a26aceee6dbaeb9eb676
susiejojo <susiejojo1@gmail.com>
2025-06-23 15:27:11 -0400

    Added cmdline arguments instead of hardcoded info

--- offline_tokenizer.py
+++ offline_tokenizer.py
@@ -1,38 +1,64 @@
-from transformers import AutoTokenizer
+import argparse
 import json
 
-# Load the tokenizer for facebook/opt-125m
-tokenizer = AutoTokenizer.from_pretrained("facebook/opt-125m", use_fast=False)
-filepath = "ShareGPT_V3_unfiltered_cleaned_split.json"
-output_filepath = "ShareGPT_V3_tokenized.json"
+from transformers import AutoTokenizer
 
-try:
-    with open(filepath, 'r', encoding='utf-8') as f:
-        data = json.load(f)
+def main():
+    parser = argparse.ArgumentParser(
+        description="A tokenizer for JSON requests in the ShareGPT format."
+        )
 
-    all_conversations = []
+    parser.add_argument(
+        "--model_name",
+        type=str,
+        help="LLM name for loading appropriate tokenizer"
+    )
+    parser.add_argument(
+        "--input_filepath",
+        type=str,
+        help="JSON filepath with text inputs and outputs for requests"
+    )
+    parser.add_argument(
+        "--output_filepath",
+        type=str,
+        help="JSON filepath to store tokenized inputs and outputs for request"
+    )
 
-    for entry in data:
-        conversations = entry["conversations"]
-        
-        # Process only the first two turns (human-gpt)
-        if len(conversations) > 1 and conversations[0]["from"] == "human" and conversations[1]["from"] == "gpt":
-            human_value = conversations[0]["value"]
-            tokenized_human_value = tokenizer.encode(human_value, return_tensors="np").tolist()[0]
-            conversations[0]["value"] = tokenized_human_value
-        
-            gpt_value = conversations[1]["value"]
-            tokenized_gpt_value = tokenizer.encode(gpt_value, return_tensors="np").tolist()[0]
-            conversations[1]["value"] = tokenized_gpt_value
+    args = parser.parse_args()
 
-            conversation_obj = {"ID": entry["id"], "conversations": []}
-            conversation_obj["conversations"].append(conversations[0])
-            conversation_obj["conversations"].append(conversations[1])
-            all_conversations.append(conversation_obj)
+    # Load the tokenizer for given model
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name, use_fast=False)
 
-    print (len(all_conversations))
-    with open(output_filepath, 'w', encoding='utf-8') as f:
-        json.dump(all_conversations, f, indent=2)
+    try:
+        with open(args.input_filepath, 'r', encoding='utf-8') as f:
+            data = json.load(f)
 
-except FileNotFoundError:
-    print(f"Error: The file at '{filepath}' was not found.")
+        all_conversations = []
+
+        for entry in data:
+            conversations = entry["conversations"]
+            
+            # Process only the first two turns (human-gpt)
+            if len(conversations) > 1 and conversations[0]["from"] == "human" and conversations[1]["from"] == "gpt":
+                human_value = conversations[0]["value"]
+                tokenized_human_value = tokenizer.encode(human_value, return_tensors="np").tolist()[0]
+                conversations[0]["value"] = tokenized_human_value
+            
+                gpt_value = conversations[1]["value"]
+                tokenized_gpt_value = tokenizer.encode(gpt_value, return_tensors="np").tolist()[0]
+                conversations[1]["value"] = tokenized_gpt_value
+
+                conversation_obj = {"ID": entry["id"], "conversations": []}
+                conversation_obj["conversations"].append(conversations[0])
+                conversation_obj["conversations"].append(conversations[1])
+                all_conversations.append(conversation_obj)
+
+        print ("Num tokenized requests:", len(all_conversations))
+        with open(args.output_filepath, 'w', encoding='utf-8') as f:
+            json.dump(all_conversations, f, indent=2)
+
+    except FileNotFoundError:
+        print(f"Error: The file at '{args.input_filepath}' was not found.")
+
+if __name__=="__main__":
+    main()
