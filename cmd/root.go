afe7601f53bb41995a24671152c1bec332cec77d
susiejojo <susiejojo1@gmail.com>
2025-06-25 10:29:31 -0400

    Added seed as CLI argument

--- cmd/root.go
+++ cmd/root.go
@@ -76,6 +76,7 @@ func Execute() {
 func init() {
 	runCmd.Flags().IntVar(&totalKVBlocks, "total-kv-blocks", 8000000, "Total number of KV cache blocks")
 	runCmd.Flags().Int64Var(&simulationHorizon, "horizon", 10000, "Total simulation horizon (in ticks)")
+	runCmd.Flags().Int64Var(&seed, "seed", 42, "Random seed")
 	runCmd.Flags().Float64Var(&rate, "rate", 0.02, "Poisson arrival rate (requests per tick)")
 	runCmd.Flags().StringVar(&logLevel, "log", "info", "Log level (trace, debug, info, warn, error, fatal, panic)")
 	runCmd.Flags().Int64Var(&stepDuration, "step", 100, "Step duration (in ticks)")
