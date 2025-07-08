929c084bf9f7c8f66673e9acef141de09ba14e39
susiejojo <susiejojo1@gmail.com>
2025-07-08 09:56:09 -0400

    Fixed conflicts in kvcache

--- sim/kvcache.go
+++ sim/kvcache.go
@@ -230,7 +230,6 @@ func (kvc *KVCacheState) AllocateKVBlocks(req *Request, startIndex int, endIndex
 		} else {
 			// KV cache is seeing this request for the first time (beginning of prefill)
 			// append the cached blocks to this request's ID map
-
 			for _, blockId := range cachedBlocks {
 				blk := kvc.Blocks[blockId]
 				blk.RefCount++
