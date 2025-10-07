97aa24d0b85dfbcc030965448a7b8f1fea83f343
Mert Toslali <toslali@ibm.com>
2025-10-07 10:43:59 -0400

    define the events

--- sim/event.go
+++ sim/event.go
@@ -38,6 +38,70 @@ func (e *ArrivalEvent) Execute(sim *Simulator) {
 	}
 }
 
+// QueuedEvent represents the queue of a new inference request into the system.
+type QueuedEvent struct {
+	time    int64    // Simulation time of queued (in ticks)
+	Request *Request // The incoming request associated with this event
+}
+
+// Timestamp returns the time of the QueuedEvent.
+func (e *QueuedEvent) Timestamp() int64 {
+	return e.time
+}
+
+// Execute does nothing
+func (e *QueuedEvent) Execute(sim *Simulator) {
+	logrus.Infof("<< Queued: %s at %d ticks", e.Request.ID, e.time)
+}
+
+// ScheduledEvent represents the scheduling of a new inference request in the system.
+type ScheduledEvent struct {
+	time    int64    // Simulation time of Scheduled (in ticks)
+	Request *Request // The incoming request associated with this event
+}
+
+// Timestamp returns the time of the ScheduledEvent.
+func (e *ScheduledEvent) Timestamp() int64 {
+	return e.time
+}
+
+// Execute does nothing
+func (e *ScheduledEvent) Execute(sim *Simulator) {
+	logrus.Infof("<< Schedule: %s at %d ticks", e.Request.ID, e.time)
+}
+
+// PreemptionEvent represents the pre-emption of an inference request in the system.
+type PreemptionEvent struct {
+	time    int64    // Simulation time of PreemptionEvent (in ticks)
+	Request *Request // The incoming request associated with this event
+}
+
+// Timestamp returns the time of the PreemptionEvent.
+func (e *PreemptionEvent) Timestamp() int64 {
+	return e.time
+}
+
+// Execute does nothing
+func (e *PreemptionEvent) Execute(sim *Simulator) {
+	logrus.Infof("<< Preemption: %s at %d ticks", e.Request.ID, e.time)
+}
+
+// RequestLeftEvent represents the leaving of an inference request from the system.
+type RequestLeftEvent struct {
+	time    int64    // Simulation time of RequestLeftEvent (in ticks)
+	Request *Request // The incoming request associated with this event
+}
+
+// Timestamp returns the time of the RequestLeftEvent.
+func (e *RequestLeftEvent) Timestamp() int64 {
+	return e.time
+}
+
+// Execute does nothing
+func (e *RequestLeftEvent) Execute(sim *Simulator) {
+	logrus.Infof("<< RequestLeft: %s at %d ticks", e.Request.ID, e.time)
+}
+
 // StepEvent represents a simulation step.
 // It encapsulates the vLLM step function, consisting of the following:
 //   - scheduler.schedule()
