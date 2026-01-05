1db179042d7e47e08d550c3bd5b1437981ac2af3
susiejojo <susiejojo1@gmail.com>
2026-01-05 14:57:13 -0500

    Roofline initial version

--- sim/event.go
+++ sim/event.go
@@ -1,8 +1,6 @@
 package sim
 
 import (
-	"fmt"
-
 	"github.com/sirupsen/logrus"
 )
 
@@ -31,7 +29,6 @@ func (e *ArrivalEvent) Execute(sim *Simulator) {
 
 	// Trigger queued event with processing delay
 	queued_delay := sim.getQueueingTime(e.Request) // coming from alpha model
-	fmt.Printf("queue delay: %v\n", queued_delay)
 	sim.Schedule(&QueuedEvent{
 		time:    e.time + queued_delay,
 		Request: e.Request,
