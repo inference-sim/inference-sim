3d79c654c2037299de0d656d57bbbf693edde7df
Dipanwita Guhathakurta <42820001+susiejojo@users.noreply.github.com>
2026-01-29 10:38:32 -0500

    Request replay and results JSON (#131)
    * Request replay and results JSON
    
    * Minor comment edit
    
    * Rm traces.csv
    
    * README update
    
    * Replay request content
    
    * Added defaults.yaml and preemption fix
    
    * Add codellama to defaults.yml
    
    * Root args bugfix

---
+++ cmd/default_config.go
@@ -0,0 +1,117 @@
+package cmd
+
+import (
+	"os"
+
+	sim "github.com/inference-sim/inference-sim/sim"
+	"github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v3"
+)
+
+// Define struct for YAML
+type WorkloadConfig struct {
+	Workloads map[string]Workload `yaml:"workloads"`
+}
+
+type Workload struct {
+	PrefixTokens      int `yaml:"prefix_tokens"`
+	PromptTokensMean  int `yaml:"prompt_tokens"`
+	PromptTokensStdev int `yaml:"prompt_tokens_stdev"`
+	PromptTokensMin   int `yaml:"prompt_tokens_min"`
+	PromptTokensMax   int `yaml:"prompt_tokens_max"`
+	OutputTokensMean  int `yaml:"output_tokens"`
+	OutputTokensStdev int `yaml:"output_tokens_stdev"`
+	OutputTokensMin   int `yaml:"output_tokens_min"`
+	OutputTokensMax   int `yaml:"output_tokens_max"`
+}
+
+// Define struct for YAML
+type Config struct {
+	Models   []Model                  `yaml:"models"`
+	Defaults map[string]DefaultConfig `yaml:"defaults"`
+	Version  string                   `yaml:"version"`
+}
+
+// Define the inner structure for default config given model
+type DefaultConfig struct {
+	GPU               string `yaml:"GPU"`
+	TensorParallelism int    `yaml:"tensor_parallelism"`
+	VLLMVersion       string `yaml:"vllm_version"`
+}
+
+type Model struct {
+	GPU               string    `yaml:"GPU"`
+	AlphaCoeffs       []float64 `yaml:"alpha_coeffs"`
+	BetaCoeffs        []float64 `yaml:"beta_coeffs"`
+	ID                string    `yaml:"id"`
+	TensorParallelism int       `yaml:"tensor_parallelism"`
+	VLLMVersion       string    `yaml:"vllm_version"`
+	TotalKVBlocks     int64     `yaml:"total_kv_blocks"`
+}
+
+func GetWorkloadConfig(workloadFilePath string, workloadType string, rate float64, maxPrompts int) *sim.GuideLLMConfig {
+	// Read YAML file
+	data, err := os.ReadFile(workloadFilePath)
+	if err != nil {
+		panic(err)
+	}
+
+	// Parse YAML
+	var cfg WorkloadConfig
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		panic(err)
+	}
+
+	if workload, workloadExists := cfg.Workloads[workloadType]; workloadExists {
+		logrus.Infof("Using preset workload %v\n", workloadType)
+		return &sim.GuideLLMConfig{Rate: rate, MaxPrompts: maxPrompts,
+			PrefixTokens: workload.PrefixTokens,
+			PromptTokens: workload.PromptTokensMean, PromptTokensStdDev: workload.PromptTokensStdev,
+			PromptTokensMin: workload.PromptTokensMin, PromptTokensMax: workload.PromptTokensMax,
+			OutputTokens: workload.OutputTokensMean, OutputTokensStdDev: workload.OutputTokensStdev,
+			OutputTokensMin: workload.OutputTokensMin, OutputTokensMax: workload.OutputTokensMax}
+	} else {
+		return nil
+	}
+}
+
+func GetDefaultSpecs(LLM string) (GPU string, TensorParallelism int, VLLMVersion string) {
+	// Read YAML file
+	data, err := os.ReadFile(defaultsFilePath)
+	if err != nil {
+		panic(err)
+	}
+
+	// Parse YAML
+	var cfg Config
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		panic(err)
+	}
+
+	if _, modelExists := cfg.Defaults[LLM]; modelExists {
+		return cfg.Defaults[LLM].GPU, cfg.Defaults[LLM].TensorParallelism, cfg.Defaults[LLM].VLLMVersion
+	} else {
+		return "", 0, ""
+	}
+}
+
+func GetCoefficients(LLM string, tp int, GPU string, vllmVersion string, defaultsFilePath string) ([]float64, []float64, int64) {
+	// Read YAML file
+	data, err := os.ReadFile(defaultsFilePath)
+	if err != nil {
+		panic(err)
+	}
+
+	// Parse YAML
+	var cfg Config
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		panic(err)
+	}
+
+	for _, model := range cfg.Models {
+		if model.ID == LLM && model.TensorParallelism == tp && model.GPU == GPU && model.VLLMVersion == vllmVersion {
+			return model.AlphaCoeffs, model.BetaCoeffs, model.TotalKVBlocks
+		}
+	}
+	return nil, nil, 0
+}
--- sim/metrics_utils.go
+++ sim/metrics_utils.go
@@ -20,6 +20,45 @@ type IntOrFloat64 interface {
 	int | int64 | float64
 }
 
+// Individual request metrics for the JSON log
+type RequestMetrics struct {
+	ArrivedAt        float64 `json:"arrived_at"`
+	ID               string  `json:"requestID"`
+	NumPrefillTokens int     `json:"num_prefill_tokens"`
+	NumDecodeTokens  int     `json:"num_decode_tokens"`
+	TTFT             float64 `json:"ttft_ms"`
+	ITL              float64 `json:"itl_ms"`
+	E2E              float64 `json:"e2e_ms"`
+	SchedulingDelay  float64 `json:"scheduling_delay_ms"`
+}
+
+// MetricsOutput defines the JSON structure for the saved metrics
+type MetricsOutput struct {
+	SimStartTimestamp     string           `json:"sim_start_timestamp"`
+	SimEndTimestamp       string           `json:"sim_end_timestamp"`
+	CompletedRequests     int              `json:"completed_requests"`
+	TotalInputTokens      int              `json:"total_input_tokens"`
+	TotalOutputTokens     int              `json:"total_output_tokens"`
+	VllmDurationSec       float64          `json:"vllm_estimated_duration_s"`
+	SimulationDurationSec float64          `json:"simulation_duration_s"`
+	ResponsesPerSec       float64          `json:"responses_per_sec"`
+	TokensPerSec          float64          `json:"tokens_per_sec"`
+	E2EMeanMs             float64          `json:"e2e_mean_ms"`
+	E2EP90Ms              float64          `json:"e2e_p90_ms"`
+	E2EP95Ms              float64          `json:"e2e_p95_ms"`
+	E2EP99Ms              float64          `json:"e2e_p99_ms"`
+	TTFTMeanMs            float64          `json:"ttft_mean_ms"`
+	TTFTP90Ms             float64          `json:"ttft_p90_ms"`
+	TTFTP95Ms             float64          `json:"ttft_p95_ms"`
+	TTFTP99Ms             float64          `json:"ttft_p99_ms"`
+	ITLMeanMs             float64          `json:"itl_mean_ms"`
+	ITLP90Ms              float64          `json:"itl_p90_ms"`
+	ITLP95Ms              float64          `json:"itl_p95_ms"`
+	ITLP99Ms              float64          `json:"itl_p99_ms"`
+	SchedulingDelayP99Ms  float64          `json:"scheduling_delay_p99_ms"`
+	Requests              []RequestMetrics `json:"requests,omitempty"`
+}
+
 // CalculatePercentile is a util function that calculates the p-th percentile of a data list
 // return values are in milliseconds
 func CalculatePercentile[T IntOrFloat64](data []T, p float64) float64 {
